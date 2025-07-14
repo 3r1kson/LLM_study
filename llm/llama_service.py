9dc4f8a16c01189b6bc456a9e5d03edf71e3ff8b
Erikson <eriksonribeiro84@gmail.com>
2025-07-14 19:51:54 -0300

    setting mlflow

--- llm/llama_service.py
+++ llm/llama_service.py
@@ -1,4 +1,7 @@
+import mlflow
+import time
 from llama_cpp import Llama
+
 
 class LLMEngine:
     def __init__(self, model_path: str):
@@ -21,12 +24,31 @@ class LLMEngine:
 
     def generate_text(self, system_msg: str, user_msg: str, max_tokens: int = 256):
         prompt = self.build_prompt(system_msg, [("user", user_msg)])
-        output = self.model(
-            prompt=prompt,
-            max_tokens=max_tokens,
-            stop=["<|user|>", "<|system|>", "</s>"],
-            temperature=0.5,
-            top_p=0.9,
-            repeat_penalty=1.15
-        )
-        return output["choices"][0]["text"].strip()
+
+        with mlflow.start_run():
+            mlflow.log_param("model_path", self.model.model_path)
+            mlflow.log_param("system_msg", system_msg)
+            mlflow.log_param("user_msg", user_msg)
+            mlflow.log_param("max_tokens", max_tokens)
+            mlflow.log_param("temperature", 0.5)
+            mlflow.log_param("top_p", 0.9)
+            mlflow.log_param("repeat_penalty", 1.15)
+
+            start_time = time.time()
+
+            output = self.model(
+                prompt=prompt,
+                max_tokens=max_tokens,
+                stop=["<|user|>", "<|system|>", "</s>"],
+                temperature=0.5,
+                top_p=0.9,
+                repeat_penalty=1.15
+            )
+            latency = time.time() - start_time
+
+            response = output["choices"][0]["text"].strip()
+
+            mlflow.log_metric("latency", latency)
+            mlflow.log_text(response, "generated_response.txt")
+
+        return response
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,18 +1,17 @@
 from services.chat_service import ChatService
 
 class MockLLM:
-    def generate_text(self, prompt: str) -> str:
-        return "Mock response to: " + prompt
-
+    def generate_text(self, prompt: str, system: str) -> str:
+        return f"Mock response to system: {system} and prompt: {prompt}"
 def test_valid_prompt():
     service = ChatService(MockLLM())
-    response = service.handle_chat("Qual é o seu nome?")
+    response = service.handle_chat(prompt="Qual é o seu nome?", system="Você é um assistente útil.")
     assert response.startswith("Mock")
 
 def test_invalid_prompt():
     service = ChatService(MockLLM())
     try:
-        service.handle_chat("Inválido")
+        service.handle_chat(prompt="Inválido", system="Você é um assistente útil.")
     except ValueError as e:
         assert str(e) == "Prompt precisa ser pergunta ou instrução"
     else:
